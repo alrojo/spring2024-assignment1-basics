f7e0c7b94e9c163f7f4c549adbe0405d0c9a9e63
Ubuntu <ubuntu@ip-172-31-91-12.ec2.internal>
2024-11-18 02:52:49 +0000

    dataloading efficiency

--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -1,9 +1,16 @@
-import os
+import os, subprocess
 from collections import defaultdict, Counter
 from typing import Dict, Optional, Tuple, Iterable, List
 import regex as re
 import json
 from cs336_basics.utils.io import PAT
+
+def count_lines_with_wc(file_path):
+    result = subprocess.run(['wc', '-l', file_path], stdout=subprocess.PIPE, text=True)
+    return int(result.stdout.split()[0])
+
+def get_percent(x):
+    return [int(x*0.01*i-1) for i in range(1,101)]
 
 def train_bpe(
     input_path: str | os.PathLike,
@@ -18,21 +25,50 @@ def train_bpe(
         vocab[i] = bytes([vocab_indexes[i-1]])
     merges = []
 
+    num_lines=count_lines_with_wc(input_path)
+    print("number of lines in file %d ..." % num_lines)
+    percentages = get_percent(num_lines)
+
     freq_table = Counter()
 
-    print("processing data line by line...")
-    with open(input_path, 'r', encoding='utf-8') as file:
-        for line in file:
-            tokens = re.findall(PAT, line)
-            for tok in tokens:
-                byte_tuple = tuple(bytes([b]) for b in tok.encode('utf-8'))
-                freq_table[byte_tuple] += 1
+    memory_efficient=False
+    pat = re.compile(PAT)
+    if memory_efficient:
+        print("processing data line by line...")
+        with open(input_path, 'r', encoding='utf-8') as file:
+            k = 0
+            for e, line in enumerate(file):
+                tokens = pat.findall(line)
+                for tok in tokens:
+                    byte_tuple = tuple(bytes([b]) for b in tok.encode('utf-8'))
+                    freq_table[byte_tuple] += 1
+                if e == percentages[k]:
+                    print("%d%%" % (k+1), end=" ")
+                    k += 1
+                    k = min(k, 99)
+        print()
+    else:
+        print("processing entire dataset ...")
+        with open(input_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+        tokens = pat.findall(content)
+        for tok in tokens:
+            byte_tuple = tuple(bytes([b]) for b in tok.encode('utf-8'))
+            freq_table[byte_tuple] += 1
     print("computing BPE merges ...")
     succesive_pairs = defaultdict(int)
     cur_merge_freq = None 
-    for bpe_seq, freq in freq_table.items():
+    num_freq_pairs = len(freq_table.items())
+    percentages = get_percent(num_freq_pairs)
+    k=0
+    for e, (bpe_seq, freq) in enumerate(freq_table.items()):
         for i in range(len(bpe_seq)-1):
             succesive_pairs[(bpe_seq[i], bpe_seq[i+1])] += freq
+        if e == percentages[k]:
+            print("%d%%" % (k+1), end=" ")
+            k += 1
+            k = min(k, 99)
+    print()
     print("merging BPE ...")
     for vocab_index in range(len(vocab), vocab_size-len(special_tokens)+1):
         if vocab_index % 100 == 0:
